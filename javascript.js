4d6e1f0a3b48f84a56ce56c3b7bcf21603d06caa
rakesh <dgloveai143@gmail.com>
2024-12-01 13:47:17 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+var a=document.querySelector("#menu");
+var b=document.querySelector(".responsive_ness");
+var check=0;
+a.addEventListener("click", function(){
+
+ if(check==0){
+   b.style="display:block; display:flex; transition:transform 0.3s;";
+   check=1;
+ }
+ else{
+    b.style.display="none";
+    check=0;
+ }
+
+});
